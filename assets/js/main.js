b27a79545e4550e893efbf9af69b6807c137889d
Hafidz25 <hafidzalif80@gmail.com>
2023-09-05 11:01:50 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+var openmenu = document.querySelector(".ri-menu-3-line");
+var sohwnav = document.querySelector(".nav-link");
+var closenav = document.querySelector(".ri-close-fill")
+openmenu.addEventListener("click",function(){
+    sohwnav.classList.add("show")
+})
+closenav.addEventListener("click", function(){
+    sohwnav.classList.remove("show")
+    closenav.style.rotate = "360deg"
+})
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
